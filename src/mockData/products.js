8904f0b4848748dc66bc14be2c07103a2208e8ee
Laksito Pamilih <laksito@pm.me>
2021-07-29 11:27:50 +0700

    add karton unit

--- src/mockData/products.js
+++ src/mockData/products.js
@@ -4,6 +4,7 @@ export default {
       id: '1',
       product_name: 'product_1',
       units: [
+        { name: 'karton', price: 100000 },
         { name: 'pak', price: 10000 },
         { name: 'pcs', price: 1000 },
       ],
@@ -12,6 +13,7 @@ export default {
       id: '2',
       product_name: 'product_2',
       units: [
+        { name: 'karton', price: 200000 },
         { name: 'pak', price: 20000 },
         { name: 'pcs', price: 2000 },
       ],
@@ -20,6 +22,7 @@ export default {
       id: '3',
       product_name: 'product_3',
       units: [
+        { name: 'karton', price: 300000 },
         { name: 'pak', price: 30000 },
         { name: 'pcs', price: 3000 },
       ],
